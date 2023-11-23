65dd371dccce59657d7d4077a48ea3bc285d6f3f
Tyler Helmrich <25161748+renoldhuman@users.noreply.github.com>
2023-11-22 22:17:18 -0500

    Added hex color conversions

--- Sources/Toolbox/Toolbox.swift
+++ Sources/Toolbox/Toolbox.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 public func validateEmail(enteredEmail:String) -> Bool {
 
@@ -6,4 +7,34 @@ public func validateEmail(enteredEmail:String) -> Bool {
     let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
     return emailPredicate.evaluate(with: enteredEmail)
 
+}
+
+@available(macOS 10.15, *)
+public func convertHexToColor(_ hex: UInt64) -> Color {
+    let red = Double((hex >> 24) & 0xFF) / 255.0
+    let green = Double((hex >> 16) & 0xFF) / 255.0
+    let blue = Double((hex >> 8) & 0xFF) / 255.0
+    let alpha = Double((hex) & 0xFF) / 255.0
+    
+    return Color(red: red, green: green, blue: blue, opacity: alpha)
+}
+
+@available(macOS 11, *)
+public func convertColorToHex(_ color: Color) -> UInt64? {
+    var hexColor: UInt64?
+    if let components = color.cgColor?.components {
+        let red = lroundf(Float(components[0]) * 255) << 24
+        let green = lroundf(Float(components[1] * 255)) << 16
+        let blue = lroundf(Float(components[2] * 255)) << 8
+        var alpha = 255
+        if components.count >= 4 {
+            alpha = lroundf(Float(components[3]) * 255)
+        }
+        
+        let pre = red | green | blue | alpha
+        
+        hexColor = UInt64(pre) & 0x00FFFFFFFFFF
+    }
+    
+    return hexColor
 }
